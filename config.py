a68f391599d3c303a6802083857a3091efd9f087
Tianfei Yu <1946135795@qq.com>
2020-06-03 09:55:58 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+pretraind_root = '/home/user/Documents/Pytorch Pretrained'
+
+res101_path = os.path.join(pretraind_root, 'ResNet', 'resnet101-5d3b4d8f.pth')
+# pretrained resnet101 from https://github.com/CSAILVision/semantic-segmentation-pytorch
+res101_csail_path = os.path.join(pretraind_root, 'ResNet', 'resnet101-imagenet.pth')
